50a04a50cb0ac2fc9ad5a8d89594f865b61a6364
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-14 11:35:44 -0500

    just some now gone migrations when trying to finalize inital schema

--- db/migrate/20151214002518_add_user_ref_to_pets.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToPets < ActiveRecord::Migration
-  def change
-    add_reference :pets, :user, index: true
-  end
-end
--- db/migrate/20151214005707_add_column_to_pets.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPets < ActiveRecord::Migration
-  def change
-    add_column :pets, :last_heartworm, :string
-  end
-end
