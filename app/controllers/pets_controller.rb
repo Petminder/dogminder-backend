5d4d82e4c24d1d2d2a30d16bc6449e4634384421
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-13 19:51:00 -0500

    adds pets controller, and adds user controller

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,58 @@
+class PetsController < ApplicationController
+  before_action :set_pet, only: [:update, :destroy]
+
+  # GET /pets
+  def index
+    if current_user
+      @pets = current_user.pets
+    else
+      @pets = pet.all
+    end
+    render json: @pets
+  end
+
+  # GET /pets/1
+  def show
+    @pet = pet.find(params[:id])
+
+    render json: @pet
+  end
+
+  # POST /pets
+  def create
+    @pet = current_user.pets.new(pet_params)
+    @pet.save
+
+    if @pet.save
+      render json: @pet, status: :created, location: @pet
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /pets/1
+  def update
+    if @pet.update(pet_params)
+      head :no_content
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /pets/1
+  def destroy
+    @pet.destroy
+
+    head :no_content
+  end
+
+  def set_pet
+    @pet = current_user.pets.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :brewery, :style, :quantity)
+  end
+
+  private :set_pet, :pet_params
+end
