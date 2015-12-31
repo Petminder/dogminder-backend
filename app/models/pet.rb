ab5690be671146469e43c9ea998b36d0febe0cd2
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-31 12:02:36 -0500

    wrong num of arugments

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,7 +4,6 @@ class Pet < ActiveRecord::Base
 
   has_attached_file :dog_pic, :dog_doc,
         :styles => { :medium => "300x300>", :thumb => "100x100>" },
-        :default_url => "images/sal.jpg",
         :path => "/images/:id/:style/:filename"
         validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 
