be8cbaa318260e7d8a77a959e03e8559c0e4e2d6
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-31 12:32:41 -0500

    working through heroku db raking issues

--- db/migrate/20151230211003_remove_column_from_pets.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnFromPets < ActiveRecord::Migration
-  def change
-    remove_column :pets, :document_file_name, :string
-    remove_column :pets, :document_updated_at, :datetime
-    remove_column :pets, :document_file_size, :integer
-    remove_column :pets, :document_content_type, :string
-  end
-end
