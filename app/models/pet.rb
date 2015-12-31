abce5284f9a4b490762eb2b5c1c4ca869669664e
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-31 12:18:16 -0500

    tries to fix error

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,8 +4,9 @@ class Pet < ActiveRecord::Base
 
   has_attached_file :dog_pic,
         :styles => { :medium => "300x300>", :thumb => "100x100>" },
+        :default_url => "images/sal.jpg",
         :path => "/images/:id/:style/:filename"
-        validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+  validates_attachment_content_type :dog_pic, :content_type => /\Aimage\/.*\Z/
 
   # By default, every file uploaded will be named 'data'.
   # This renames the file to a timestamp, preventing name collisions.
@@ -14,16 +15,16 @@ class Pet < ActiveRecord::Base
     self.dog_pic.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
   end
 
-  before_post_process :rename_dog_pic, :rename_dog_doc
+  before_post_process :rename_dog_pic
 
-# has_attached_file :dog_doc,
-#       :path => "/documents/:id/:filename"
-#   validates_attachment :dog_doc, :content_type => { :content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+has_attached_file :dog_doc,
+      :path => "/documents/:id/:filename"
+  validates_attachment :dog_doc, :content_type => { :content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
-#   def rename_dog_doc
-#     self.dog_doc.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
-#   end
+  def rename_dog_doc
+    self.dog_doc.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
+  end
 
-#   before_post_process :rename_dog_doc
+  before_post_process :rename_dog_doc
 
 end
