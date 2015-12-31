6732022aa93b9ad5c41017c8371d57401842aba1
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-31 12:04:32 -0500

    working through arguments error on has_atttached_file to in pet model for image and file upload

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ActiveRecord::Base
   belongs_to :user
   has_many :documents
 
-  has_attached_file :dog_pic, :dog_doc,
+  has_attached_file :dog_pic,
         :styles => { :medium => "300x300>", :thumb => "100x100>" },
         :path => "/images/:id/:style/:filename"
         validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
@@ -14,7 +14,7 @@ class Pet < ActiveRecord::Base
     self.dog_pic.instance_write :file_name, "#{Time.now.to_i.to_s}.png"
   end
 
-  before_post_process :rename_dog_pic, :rename_dog_doc
+  before_post_process :rename_dog_pic
 
 # has_attached_file :dog_doc,
 #       :path => "/documents/:id/:filename"
